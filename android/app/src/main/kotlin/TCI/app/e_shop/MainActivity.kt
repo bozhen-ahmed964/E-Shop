f0eee3e59e867b900f1c1b43ccb1c94424e30b65
shayda <final.project336699@gmail.com>
2022-10-25 02:27:59 +0300

    first commit

---
+++ android/app/src/main/kotlin/TCI/app/e_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package TCI.app.e_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const E_Shop());
+}
+
+class E_Shop extends StatelessWidget {
+  const E_Shop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "E-Shop",
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.green,
+        ));
+  }
+}
